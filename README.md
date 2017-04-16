1a85cd19d9be619302bd91e7ec14932e1f03665c
Kristian Mandrup <kmandrup@gmail.com>
2017-04-16 08:56:22 +0200

    initial

---
+++ README.md
@@ -0,0 +1,158 @@
+# Chevrotain Rule DSL
+
+Chevrotain Rule DSL
+
+## Install
+
+`npm i -S chevrotain-rule-dsl`
+
+## Usage
+
+The usual approach:
+
+```js
+$.RULE("qualifiedName", () => {
+    $.CONSUME(Identifier)
+    $.CONSUME(Dot)
+    $.CONSUME2(Identifier)
+})
+```
+
+Much cleaner expressiveness with the DSL
+
+```js
+import { rule } from './rule-parser'
+import {
+    allTokens,
+    Comma,
+    Identifier,
+    // ...
+} from './lexer'
+
+export class SelectParser extends Parser {
+  registry = {} // to hold registered rules
+
+  constructor(input) {
+      super(input, allTokens)
+      // must be called at the end of the constructor!
+      Parser.performSelfAnalysis(this)
+  }
+
+
+  selectClause = rule(this, 'selectClause', [
+      // auto-matically detect token and use consume:
+      Select, {
+          // automatically detect REPEAT if min or sep
+          min: 1,
+          sep: Comma,
+          def: Identifier
+      }
+  ])
+
+  selectStatement = rule(this, 'selectStatement', [
+      'selectClause', 'fromClause', {
+          option: ['whereClause']
+      }
+  ])
+
+  fromClause = rule(this, 'fromClause', [{
+      consume: From
+  }, {
+      consume: Identifier
+  }])
+
+  whereClause = rule(this, 'whereClause', [Where, 'expression'])
+
+  atomicExpression = rule(this, 'atomicExpression', {
+      // automatically use alt for each OR rule
+      or: [Integer, Identifier]
+  })
+
+  relationalOperator = rule(this, 'relationalOperator', [{
+      or: [GreaterThan, LessThan]
+  }])
+
+  expression = rule(this, 'expression', [
+      'atomicExpression',
+      'relationalOperator',
+      // auto-detect repeat!
+      'atomicExpression'
+  ])
+```
+
+## Status
+
+Currently the DSL is parsed in order to build a function that when executed is equivalent
+to using the standard API. In turn we also generate code that mirrors using the standard API, so that the built in GAST parser works (walking over the code using Regular expressions).
+
+Next step will be to also build the GAST directly (see below).
+
+### CST
+
+[Concrete Syntax Tree Creation](https://github.com/SAP/chevrotain/blob/master/docs/concrete_syntax_tree.md)
+
+Chevrotain has the capability to automatically create a concrete syntax tree (CST) during parsing. A CST is a simple structure which represents the entire parse tree. It contains information on every token parsed.
+
+The main advantage of using the automatic CST creation is that it enables writing "pure" grammars. This means that the semantic actions are not embedded into the grammar implementation but are instead completely separated from it.
+
+This separation of concerns makes the grammar easier to maintain and makes it easier to implement different capabilities on the grammar, for example: separate logic for compilation and for IDE support.
+
+CST is enabled by setting the `outputCst` flag.
+
+```js
+class MyParser extends chevrotain.Parser {
+     constructor(input) {
+        super(input, allTokens, {outputCst : true})
+    }
+}
+```
+
+Using:
+
+```js
+$.RULE("qualifiedName", () => {
+    $.CONSUME(Identifier)
+    $.CONSUME(Dot)
+    $.CONSUME2(Identifier)
+})
+
+input = "foo.bar"
+
+output = {
+  name: "qualifiedName",
+  children: {
+      Dot : ["."],
+      Identifier : ["foo", "bar"]
+  }
+}
+```
+
+## Transform CST to AST
+
+[CST to AST example](https://github.com/kdex/chi/blob/master/src/Parser.js#L249)
+
+## Contributing
+
+Install dependency modules/packages
+
+`npm i`
+
+### Compile/Build
+
+The project includes a `gulpfile` configured to use Babel 6.
+All `/src` files are compiled to `/dist` including source maps.
+
+Scripts:
+
+- start: `npm start`
+- build: `npm run build` (ie. compile)
+- watch and start: `npm run watch`
+- watch and build: `npm run watch:b`
+
+### Run Tests
+
+`npm test` or simply `ava test`
+
+## License
+
+MIT Kristian Mandrup
---
+++ src/index.ts
@@ -0,0 +1,257 @@
+import {
+  Parser,
+  Token
+} from 'chevrotain'
+
+export interface IResult {
+  rule: any
+  code: string
+}
+
+function codeOf(value) {
+  return typeof value === 'function' ? value.name : new String(value)
+}
+
+export class RuleParser {
+  static registry = {}
+  usedRules = {}
+  _registry = {}
+  code = []
+  public codeStr: string
+  logging: boolean
+  $: any
+
+  constructor(parser, options = { logging: false, registry: null }) {
+    if (!(parser instanceof Parser)) {
+      console.error('parser', parser)
+      throw new Error('RuleParser must be created with a Parser instance')
+    }
+    this.$ = parser
+    this.usedRules = {}
+    this.logging = options.logging
+    this._registry = parser['registry'] || options.registry || RuleParser.registry
+  }
+
+  parse(rule, options = {}): IResult {
+    this.log('parse', rule, options)
+
+    if (Array.isArray(rule)) {
+      return this.parseList(rule, options)
+    }
+    if (rule.prototype instanceof Token) {
+      this.log('consume since rule is token', rule)
+      return this.consume(rule)
+    }
+
+    if (typeof rule === 'object') {
+      return this.parseObj(rule, options)
+    }
+    // if string, always assume subrule
+    if (typeof rule === 'string') {
+      return this.subrule(rule)
+    }
+    if (typeof rule === 'function') {
+      return { rule, code: rule.name }
+    }
+    throw new Error(`Invalid rule(s) ${typeof rule}`)
+  }
+
+  protected parseList(rules, options = {}): IResult {
+    this.log('parseList', rules, options)
+    let parsedRules = rules.map(rule => this.parse(rule, options))
+    let codeStmts = parsedRules.map(pr => pr.code).join('\n')
+
+    this.log('parsedRules', parsedRules)
+    this.log('codeStmts', codeStmts)
+    let rule = () => {
+      parsedRules.map(pr => pr.rule())
+    }
+    this.log('rule', rule)
+    let code = '() => {\n' + codeStmts + '\n}\n'
+    let result = {
+      rule,
+      code
+    }
+    this.log('parsedList', result)
+    return result
+  }
+
+  protected parseObj(rule, options = {}): IResult {
+    this.log('parseObj', rule, options)
+
+    function isRepeat(value) {
+      return value.repeat || value.sep || value.min || value.def
+    }
+    function isAlt(value) {
+      return value && value.parent === 'or'
+    }
+
+    if (isAlt(options)) {
+      return this.alt(rule)
+    }
+
+    if (isRepeat(rule)) {
+      return this.repeat(rule)
+    }
+
+    let key = Object.keys(rule)[0]
+    let value = rule[key]
+
+    switch (key) {
+      case 'rule':
+        return this.subrule(value)
+      case 'rule2':
+        return this.subrule(value, 'SUBRULE2')
+      case 'option':
+        return this.option(value)
+      case 'consume':
+        return this.consume(value)
+      case 'repeat':
+        return this.repeat(value)
+      case 'alt':
+        return this.alt(value)
+      case 'or':
+        return this.or(value)
+      default:
+        throw new Error(`Unknown key in rule object: ${key}`)
+    }
+  }
+
+  log(msg, ...args) {
+    if (this.logging) {
+      console.log(msg, ...args)
+    }
+  }
+
+  findRule(name) {
+    return this.registry[name]
+  }
+
+  get registry() {
+    return this._registry
+  }
+
+  register(name, rule) {
+    this.registry[name] = rule
+  }
+
+  addCode(ruleCode) {
+    this.code.push(ruleCode)
+  }
+
+  subrule(value, fun = 'SUBRULE') {
+    this.log('subrule', value)
+    let _rule = (typeof value === 'string') ? this.findRule(value) : value
+    if (typeof _rule !== 'function') {
+      console.warn('Not yet registered, evaluate later...', _rule, value, this.findRule(value), Object.keys(this.registry))
+      // throw new Error(`subrule must be function, was ${typeof rule}`)
+    }
+    // auto-detect reuse of subrule!
+    if (this.usedRules[fun]) {
+      this.log('repeat rule')
+      fun = 'SUBRULE2'
+    }
+    this.usedRules[fun] = true
+    let code = `$.${fun}(` + _rule + ')'
+    let rule = () => this.$[fun](rule)
+    return { rule, code }
+  }
+
+  // must be a Token
+  protected consume(value): IResult {
+    this.log('consume', value)
+    let code = '$.CONSUME(' + codeOf(value) + ')'
+    let $ = this.$
+    let rule = () => $.CONSUME(value).bind($)
+    let result = { rule, code }
+    this.log('consumed', result)
+    return result
+  }
+
+  protected alt(value): IResult {
+    this.log('alt', value)
+    let parsedRule = this.parse(value)
+    let code = '{ALT: ' + parsedRule.code + '}'
+    let rule = { ALT: parsedRule.rule }
+    return { rule, code }
+  }
+
+  protected repeat(value): IResult {
+    this.log('repeat', value)
+    let rep = {
+      SEP: '',
+      DEF: () => { }
+    }
+    let prefix = value.min > 0 ? 'AT_LEAST_ONE' : 'MANY'
+    let postfix = value.sep ? 'SEP' : null
+    let fun = postfix ? [prefix, postfix].join('_') : prefix
+
+    this.log('type', fun)
+
+    rep.SEP = value.sep || value.separator
+    this.log('separator', rep.SEP)
+
+    let def = value.rule || value.def
+    this.log('def:', def)
+    let definition = this.parse(def)
+    this.log('definition', definition)
+
+    rep.DEF = () => definition.rule
+    let codeRep = {
+      SEP: rep.SEP,
+      DEF: definition.code
+    }
+    this.log('repeat rule:', fun, rep)
+    let rule = this.$[fun](rep)
+    let code = (`$.${fun}(` + codeRep + ')')
+    return { rule, code }
+  }
+
+  protected or(alternatives): IResult {
+    this.log('or', alternatives)
+    let parsed = this.parseList(alternatives, { parent: 'or' })
+    let code = '$.OR([' + parsed.code + '])'
+    let rule = () => this.$.OR(parsed.rule)
+    return { rule, code }
+  }
+
+  protected option(value): IResult {
+    this.log('option', value)
+    let _rule = (typeof value === 'string') ? this.findRule[value] : value
+    let parsed = this.parse(_rule)
+    if (typeof parsed.rule !== 'function') {
+      throw new Error(`option must be function, was ${typeof parsed.rule}`)
+    }
+
+    let rule = () => this.$.OPTION(parsed.rule)
+    let code = parsed.code
+    return { rule, code }
+  }
+
+  protected rule(name, rules, config): Function {
+    this.log('rule', name, rules)
+    if (typeof name !== 'string') {
+      throw new Error(`rule name must be a valid name (string), was ${name}`)
+    }
+    let $ = this.$
+    let rule = () => $.RULE(name, rules.rule, config).bind($)
+    return rule
+  }
+
+  public createRule(name: string, rules, options): Function {
+    options = options || {}
+    let parsed = this.parse(rules, options)
+    this.log('createRule: parsedRule', parsed.rule)
+    options.code = options.code || parsed.code
+    this.codeStr = parsed.code
+    this.log('createRule: parsedCode', parsed.code)
+    let parsedRule = this.rule(name, parsed.rule, options)
+    this.log('createRule: rule', parsedRule)
+    this.register(name, parsedRule)
+    return parsedRule
+  }
+}
+
+export function rule(parser, name: string, rules, options?): Function {
+  return new RuleParser(parser, options).createRule(name, rules, options)
+}
